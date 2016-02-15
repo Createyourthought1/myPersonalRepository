918d0bd9608e461c22eb59ae3a18b907305474a0
Chanchal Narang <chanchal.narang@t-mobile.com>
2016-02-15 18:12:16 +0530

    pushing new user to array

--- shopping Cart/imageFactory.js
+++ shopping Cart/imageFactory.js
@@ -1,15 +1,20 @@
 var app=angular.module("shoppingCart",[]);
 
 app.factory("imageFactory",function($http){
-    
+  var compiledData=[];
    var jsonData=function(response){
        
        $http.get("imageFile.json").success(response);
    } ;
-        
+       
+    var pushArray=function(user){
+        compiledData.push(user);
+        console.log(compiledData.length);
+        return(compiledData);
+    };
     return{
-     jsonData:jsonData   
-        
+     jsonData:jsonData ,  
+    pushArray: pushArray
     }   ;
  
     
--- shopping Cart/shoppingCart-controller.js
+++ shopping Cart/shoppingCart-controller.js
@@ -1,14 +1,24 @@
 
 app.controller("shoppingCart-controller",function(imageFactory,$scope){
-  
-    
+   
+    $scope.user={
+        name:" ",
+        password:"",
+        email:" "
+    }
+   
     imageFactory.jsonData(function(response){
        
         $scope.image=response;
         console.log($scope.image[0].imageUrl);
     });
     
-    
+  $scope.pushUserData=function(){ 
+$scope.compiledData=imageFactory.pushArray($scope.user);
+      $scope.user=null;
+      
+};
+   
     
     
 });
